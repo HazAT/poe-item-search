d6d5442e11c20d2e7c74f3ceb5a61b64b8ffb95b
Daniel Griesser <daniel.griesser.86@gmail.com>
2024-12-14 08:42:43 +0100

    Fix tests

--- src/item.test.js
+++ src/item.test.js
@@ -68,8 +68,60 @@ test("matchStatsOnItem", () => {
   const regexStats = addRegexToStats(stats);
   expect(matchStatsOnItem(gloves1, regexStats)).toStrictEqual([
     {
-      id: "explicit.stat_1873752457",
-      value: { min: "0.25" },
+      id: "explicit.stat_1671376347",
+      regex:
+        /(?:\+|-)?(\d+(?:.\d+)?)?% to (?:Resistances|Lightning Resistance)/,
+      text: "#% to [Resistances|Lightning Resistance]",
+      type: "explicit",
+      value: {
+        min: "12",
+      },
+    },
+    {
+      id: "explicit.stat_4220027924",
+      regex: /(?:\+|-)?(\d+(?:.\d+)?)?% to (?:Resistances|Cold Resistance)/,
+      text: "#% to [Resistances|Cold Resistance]",
+      type: "explicit",
+      value: {
+        min: "6",
+      },
+    },
+    {
+      id: "explicit.stat_691932474",
+      regex: /(?:\+|-)?(\d+(?:.\d+)?)? to (?:Accuracy|Accuracy) Rating/,
+      text: "# to [Accuracy|Accuracy] Rating",
+      type: "explicit",
+      value: {
+        min: "339",
+      },
+    },
+    {
+      id: "explicit.stat_1368271171",
+      regex: /Gain (?:\+|-)?(\d+(?:.\d+)?)? Mana per Enemy Killed/,
+      text: "Gain # Mana per Enemy Killed",
+      type: "explicit",
+      value: {
+        min: "3",
+      },
+    },
+    {
+      id: "explicit.stat_4015621042",
+      regex:
+        /(?:\+|-)?(\d+(?:.\d+)?)?% increased (?:EnergyShield|Energy Shield)/,
+      text: "#% increased [EnergyShield|Energy Shield]",
+      type: "explicit",
+      value: {
+        min: "60",
+      },
+    },
+    {
+      id: "explicit.stat_3299347043",
+      regex: /(?:\+|-)?(\d+(?:.\d+)?)? to maximum Life/,
+      text: "# to maximum Life",
+      type: "explicit",
+      value: {
+        min: "113",
+      },
     },
   ]);
 });
